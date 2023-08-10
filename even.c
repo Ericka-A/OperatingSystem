1e104b4f761805cec892273c220827040f6616a1
Giridhar Chettiar <giridharchettiar@aceik.com.au>
2023-08-10 11:48:17 +1000

    commiting the changes

--- even.c
+++ even.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <signal.h>
 #include <unistd.h>
 
-volatile int hup_received = 0;
-volatile int int_received = 0;
 
-void *signalHandlerThread(void *arg) {
-    while (1) {
-        if (hup_received) {
-            printf("Ouch!\n");
-            break;
-        }
-        if (int_received) {
-            printf("Yeah!\n");
-            break;
-        }
-        usleep(100000);  // Sleep for 0.1 seconds
-    }
-    return NULL;
+void hupHandler(int signum) {
+   printf("Ouch!\n");
+}
+
+void intHandler(int signum) {
+    printf("Yeah!\n");
 }
 
 int main(int argc, char *argv[]) {
+    signal(SIGHUP, hupHandler);
+    signal(SIGINT, intHandler);
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <n>\n", argv[0]);
         return 1;
@@ -33,29 +27,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, signalHandlerThread, NULL);
-
     int evenNumber = 0;
 
-    while (n > 0 && !hup_received && !int_received) {
+    while (n > 0) {
         printf("%d\n", evenNumber);
         fflush(stdout);
         sleep(5);
         evenNumber += 2;
         n--;
     }
-
-    // Set the corresponding flag to simulate signal handling
-    if (hup_received) {
-        hup_received = 0;
-    }
-
-    if (int_received) {
-        int_received = 0;
-    }
-
-    pthread_join(thread, NULL);
 
     return 0;
 }

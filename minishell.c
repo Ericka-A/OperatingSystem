0cadeaf009d898eba56eb38ba07b9b63ba8078a2
Giridhar Chettiar <giridharchettiar@aceik.com.au>
2023-08-12 20:20:01 +1000

    minishell commit

--- minishell.c
+++ minishell.c
@@ -13,121 +13,169 @@
 
 int pidChild;		 /* value returned by fork sys call */
 char line[NL];          /* command input buffer */
-char lineCopyBackup[NL];
+char lineCopy[NL];
 int backgroundCounter = 0;
 
 typedef enum {
-    NotAssigned,
-    Running,
+    UnAssigned,
+    ProcessRunning,
     Completed
-} BackgroundEnumStatus;
+} BackgroundStatusEnum;
 
 typedef struct {
     int pid;
     int counter;
     char command[NL];
-    BackgroundEnumStatus status;
+    BackgroundStatusEnum status;
 } BackgroundProcess;
 
 BackgroundProcess backgroundProcess[MB];
 
-void printBackgroundDoneAndClearProcess(int idx) {
-    fprintf(stdout, "[%d]+ Done %s\n", backgroundProcess[idx].counter, backgroundProcess[idx].command);
-    backgroundProcess[idx].status = NotAssigned;
+void printBackgroundProcessDoneAndProcess(int index) {
+    fprintf(stdout, "[%d]+ Done %s\n", backgroundProcess[index].counter, backgroundProcess[index].command);
+    backgroundProcess[index].pid = -1;
+    backgroundProcess[index].counter = -1;
+    backgroundProcess[index].status = UnAssigned;
+    strncpy(backgroundProcess[index].command, "", NL);
 }
 
 void checkForCompletedbackgroundProcess() {
-    for (int i = 1; i <= backgroundCounter; ++i) {
-        if (backgroundProcess[i].status != NotAssigned) {
+    int controlVariable = 0;
+
+    for (int i = 0; i < MB; ++i) {
+        if (backgroundProcess[i].pid != -1 && backgroundProcess[i].status != UnAssigned) {
+
             int status;
-            int result = waitpid(backgroundProcess[i].pid, &status, WNOHANG);
+            int result = waitpid(backgroundProcess[i].pid, &status, WNOHANG); //extern __pid_t waitpid (__pid_t __pid, int *__stat_loc, int __options);
             if (result > 0) {
-                printBackgroundDoneAndClearProcess(i);
+                printBackgroundProcessDoneAndProcess(i);
+            }
+            if (backgroundProcess[i].status == ProcessRunning) {
+                controlVariable++;
             }
         }
     }
+    if (controlVariable == 0) {
+        fflush(stdout);
+        backgroundCounter = 0;
+    }
 }
 
-int main(int argc, char *argv[], char *envp[]) {
+void prompt(void)
+{
+    // fprintf(stdout, "\n msh> ");
+    // fflush(stdout);
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+    int childPid;
     char *v[NV];
     char *sep = " \t\n";
     bool isBackground;
 
-    while (1) {
-        //prompt();
+    while (1)
+    {
+        prompt();
         fgets(line, NL, stdin);
         fflush(stdin);
 
-        if (feof(stdin)) {
+        if (feof(stdin))
+        {
             printf("\nEnd of file reached.\n");
             exit(0);
         }
+
         if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
             continue;
 
         for (int i = 0; i < NL; i++) {
-            lineCopyBackup[i] = line[i];
+            lineCopy[i] = line[i];
         }
 
         v[0] = strtok(line, sep);
 
         int i;
-        for (i = 1; i < NV; i++) {
+        for (i = 1; i < NV; i++)
+        {
             v[i] = strtok(NULL, sep);
+
             if (v[i] == NULL)
                 break;
         }
 
-        if (strcmp(v[0], "cd") == 0) {
-            if (v[1] != NULL) {
-                if (chdir(v[1]) != 0) {
+        if (strcmp(v[0], "cd") == 0)
+        {
+            if (v[1] != NULL)
+            {
+                if (chdir(v[1]) != 0)
+                {
                     continue;
                 }
-            } else {
+            }
+            else
+            {
                 continue;
             }
-        } else {
+        }
+        else
+        {
             isBackground = (strcmp(v[i - 1], "&") == 0);
-
-            if (isBackground) {
-                backgroundCounter++;
+            if (isBackground)
+            {
                 isBackground = true;
                 v[i - 1] = NULL;
 
-                int len = strlen(lineCopyBackup);
-                if (len - 3 >= 0) {
-                    lineCopyBackup[len - 3] = '\0';
-                } else if (len - 2 >= 0) {
-                    lineCopyBackup[len - 2] = '\0';
+                int len = strlen(lineCopy);
+                if (len - 3 >= 0)
+                {
+                    lineCopy[len - 3] = '\0';
                 }
+                else if (len - 2 >= 0)
+                {
+                    lineCopy[len - 2] = '\0';
+                }
+
             }
 
-            switch (pidChild =fork()) {
-                case -1: /* fork returns error to parent process */
+            switch (childPid = fork())
+            {
+                case -1:
                 {
                     break;
                 }
-                case 0: /* code executed only by child process */
+                case 0:
                 {
-                    if (isBackground) {
-                        printf("[%d] %d\n", backgroundCounter, getpid()); //formed this logic with help of Geeks for Geeks and chatGpt
+                    if (isBackground)
+                    {
+                        backgroundCounter++;
+                        printf("[%d] %d\n", backgroundCounter, getpid()); //extern __pid_t getpid (void) __THROW;
                         fflush(stdout);
+
                         backgroundProcess[backgroundCounter].pid = getpid();
                         backgroundProcess[backgroundCounter].counter = backgroundCounter;
-                        strncpy(backgroundProcess[backgroundCounter].command, lineCopyBackup, NL);
-                        backgroundProcess[backgroundCounter].status = Running;
+                        strncpy(backgroundProcess[backgroundCounter].command, lineCopy, NL);
+                        backgroundProcess[backgroundCounter].status = ProcessRunning;
                     }
-
                     execvp(v[0], v);
                     exit(0);
                 }
-                default: /* code executed only by parent process */
+                default:
                 {
-                    if (!isBackground) {
-						int status;
-						waitpid(pidChild, &status, 0); 
-						checkForCompletedbackgroundProcess(); 
-                    } else {
+                    if (!isBackground)
+                    {
+                        int status;
+                        waitpid(childPid, &status, 0);
+                        checkForCompletedbackgroundProcess();
+                    }
+                    else
+                    {
+                        backgroundCounter++;
+                        backgroundProcess[backgroundCounter].pid = childPid;
+                        backgroundProcess[backgroundCounter].counter = backgroundCounter;
+                        strncpy(backgroundProcess[backgroundCounter].command, lineCopy, NL);
+                        backgroundProcess[backgroundCounter].status = ProcessRunning;
+
                         checkForCompletedbackgroundProcess();
                     }
                     break;
